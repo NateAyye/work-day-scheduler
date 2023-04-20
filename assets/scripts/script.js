5811d3f2606e3e18c74f5aef1d199f63d9ce95e3
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-04-20 16:51:52 -0700

    Added the getData and saveData functions for fetching the localStorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,8 @@
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 dayjs.extend(window.dayjs_plugin_updateLocale);
 
+const today = dayjs();
+
 // function for adding the ordinal with the day so that it has the correct syntax
 // Function gotten off of a github issue since the ordinal was not working the way it was supposed to
 // Github Issue: https://github.com/iamkun/dayjs/issues/1891
@@ -13,17 +15,33 @@ dayjs.updateLocale('en', {
   },
 });
 
+// Function for getting the saved Data
+function getData(currentDay) {
+  const data = localStorage.getItem(currentDay.format('MMMM DD, YYYY'));
+  return data ? JSON.parse(data) : data;
+}
+
+function setData(hour, data) {
+  const localStore = localStorage.getItem(today.format('MMMM DD, YYYY'));
+  const storage = localStore ? JSON.parse(localStore) : {};
+
+  storage[hour] = data.trim();
+  localStorage.setItem(today.format('MMMM DD, YYYY'), JSON.stringify(storage));
+}
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  const currentDay = dayjs();
+  const workDay = dayjs().hour(8).minute(0).second(0);
+  const timeblocks = getData(currentDay);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -34,6 +52,7 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(currentDay.format('dddd MMMM, Do'));
 });
